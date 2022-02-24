3799d932ec66577b56d0c53f57ba8b19839f8c80
Pat Patterson <pat@backblaze.com>
2022-02-24 12:12:52 -0800

    Initial code

---
+++ README.md
@@ -0,0 +1,67 @@
+# Cloudflare Worker B2 Webhook
+
+Proxy Backblaze S3 compatible API requests, sending notifications to a webhook.
+
+* Incoming requests must be signed with the same credentials that you configure in the worker. The worker validates the AWS V4 signature on all downstream (incoming) requests and then signs the upstream (outgoing) request.
+* Notifications are dispatched asynchronously to avoid delaying the response to the client.
+
+_Based on [https://github.com/obezuk/worker-signed-s3-template](https://github.com/obezuk/worker-signed-s3-template)_
+
+You can use any S3 SDK or CLI to send requests as long as you set the endpoint URL to the worker endpoint:
+
+```bash
+% export AWS_ACCESS_KEY_ID=<your b2 application key id>
+% export AWS_SECRET_ACCESS_KEY=<your b2 application key> 
+% aws s3 cp --endpoint-url https://cf-b2-webhook.<your-subdomain>.workers.dev hello.txt s3://<your-bucket-name>/hello.txt
+upload: hello.txt to s3://<your-bucket-name>/hello.txt
+```
+
+## Configuration
+
+You must configure the following values in `wrangler.toml`
+
+```toml
+[vars]
+AWS_ACCESS_KEY_ID = "<your b2 application key id>"
+AWS_S3_ENDPOINT = "<your S3 endpoint - e.g. s3.us-west-001.backblazeb2.com >"
+WEBHOOK_URL = "<e.g. https://api.example.com/webhook/1 >"
+```
+
+You must also configure `AWS_SECRET_ACCESS_KEY` as a secret:
+
+```bash
+echo "<your b2 application key>" | wrangler secret put AWS_SECRET_ACCESS_KEY
+```
+
+## Webhook Notification
+
+The worker POSTs a JSON payload to the configured webhook for each request that it processes. For example:
+
+```json
+{
+  "contentLength": "14",
+  "contentType": "text/plain",
+  "method": "PUT",
+  "signatureTimestamp": "20220224T193204Z",
+  "status": 200,
+  "url": "https://s3.us-west-004.backblazeb2.com/my-private-bucket/tester.txt"
+}
+```
+
+You can customize the `handleRequest()` function to add additional data as you require.
+
+## Limitations
+
+The entire request, including its body, must fit into available memory. [Cloudflare Workers have 128MB available for use](https://developers.cloudflare.com/workers/platform/limits#worker-limits), of which about 30MB is used by the system. Objects larger than about 80-90MB should be processed in multiple parts.
+
+#### Wrangler
+
+You can use this repository as a template for your own worker using [`wrangler`](https://github.com/cloudflare/wrangler):
+
+```bash
+wrangler generate projectname https://github.com/Backblaze-B2-Samples/cf-b2-webhook
+```
+
+#### Serverless
+
+To deploy using serverless add a [`serverless.yml`](https://serverless.com/framework/docs/providers/cloudflare/) file.
---
+++ index.js
@@ -0,0 +1,159 @@
+//
+// Proxy Backblaze S3 compatible API requests, sending notifications to a webhook
+//
+// Adapted from https://github.com/obezuk/worker-signed-s3-template
+//
+import { AwsClient } from 'aws4fetch'
+
+// Extract the region from the endpoint
+
+const endpointRegex = /^s3\.([a-zA-Z0-9-]+)\.backblazeb2\.com$/;
+const [ , aws_region] = AWS_S3_ENDPOINT.match(endpointRegex);
+
+const aws = new AwsClient({
+    "accessKeyId": AWS_ACCESS_KEY_ID,
+    "secretAccessKey": AWS_SECRET_ACCESS_KEY,
+    "region": aws_region
+});
+
+
+addEventListener('fetch', function(event) {
+    event.respondWith(handleRequest(event))
+});
+
+
+// Request body is a ReadableStream - aws4fetch needs an ArrayBuffer
+function toArrayBuffer(body) {
+    // Returns a promise that resolves with an ArrayBuffer.
+    return body ? (new Response(body)).arrayBuffer() : undefined;
+}
+
+
+// These headers appear in the request, but are not passed upstream
+const UNSIGNABLE_HEADERS = [
+    'x-forwarded-proto',
+    'x-real-ip',
+]
+
+
+// Filter out cf-* and any other headers we don't want to include in the signature
+function filterHeaders(headers) {
+    return Array.from(headers.entries())
+      .filter(pair => !UNSIGNABLE_HEADERS.includes(pair[0]) && !pair[0].startsWith('cf-'));
+}
+
+
+// Verify the signature on the incoming request
+async function verifySignature(request, body) {
+    const authorization = request.headers.get('Authorization');
+    if (!authorization) {
+        return false;
+    }
+
+    // Parse the AWS V4 signature value
+    const re = /^AWS4-HMAC-SHA256 Credential=([^,]+),\s*SignedHeaders=([^,]+),\s*Signature=(.+)$/;
+    let [ , credential, signedHeaders, signature] = authorization.match(re);
+
+    credential = credential.split('/');
+    signedHeaders = signedHeaders.split(';');
+
+    // Verify that the request was signed with the expected key
+    if (credential[0] != AWS_ACCESS_KEY_ID) {
+        return false;
+    }
+
+    // Use the timestamp from the incoming signature
+    const datetime = request.headers.get('x-amz-date');
+
+    // Extract the headers that we want from the complete set of incoming headers
+    const headersToSign = signedHeaders
+        .map(key => ({
+            name: key, 
+            value: request.headers.get(key) 
+        }))
+        .reduce((obj, item) => (obj[item.name] = item.value, obj), {});
+
+    const signedRequest = await aws.sign(request.url, {
+        method: request.method,
+        headers: headersToSign,
+        body: body,
+        aws: { datetime: datetime }
+    });
+
+    // All we need is the signature component of the Authorization header
+    const [ , , , generatedSignature] = request.headers.get('Authorization').match(re);
+
+    return signature === generatedSignature;
+}
+
+
+// Could add more detail regarding the specific error, but this enough for now
+function errorResponse() {
+    return new Response(
+`<ErrorResponse xmlns="https://iam.amazonaws.com/doc/2010-05-08/">
+  <Error>
+    <Type>Sender</Type>
+    <Code>SignatureDoesNotMatch</Code>
+    <Message>Signature validation failed.</Message>
+  </Error>
+  <RequestId>0300D815-9252-41E5-B587-F189759A21BF</RequestId>
+</ErrorResponse>`, 
+        { status: 403 });
+}
+
+
+
+// Where the magic happens...
+async function handleRequest(event) {
+    const request = event.request;
+
+    // Set upstream target hostname.
+    var url = new URL(request.url);
+    url.hostname = AWS_S3_ENDPOINT;
+
+    // Read the body into memory
+    // Note - Cloudflare Workers have 128MB available to them, of which
+    // the system uses about 30MB. Any payload bigger than about 95MB
+    // will trigger an out of memory error at this point!
+    const body = await toArrayBuffer(request.body);
+
+    // Only handle requests signed by our configured key.
+    if (!await verifySignature(request, body)) {
+        return errorResponse();
+    }
+
+    // Certain headers appear in the incoming request but are
+    // removed from the outgoing request. If they are in the
+    // signed headers, B2 can't validate it.
+    const headers = filterHeaders(request.headers);
+
+    var signedRequest = await aws.sign(url, {
+        method: request.method,
+        headers: headers,
+        body: body
+    });
+
+    // Wait for the upstream response
+    const response = await fetch(signedRequest);
+
+    // This will fire the fetch to the webhook asynchronously so the
+    // response is not delayed.
+    event.waitUntil(
+      fetch(WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            contentLength: request.headers.get('content-length'),
+            contentType: request.headers.get('content-type'),
+            method: request.method,
+            signatureTimestamp: request.headers.get('x-amz-date'),
+            status: response.status,
+            url: response.url,
+        })
+      })    
+    );
+
+    return response;
+}
